46fc7bbb68451002050936bf356d41867063790f
Jimmy Nguyen <nphumy89@gmail.com>
2019-05-27 06:46:54 +1200

    Create Object Message

---
+++ jmsfundamentals/src/main/java/com/bharath/jms/messagestructure/Patient.java
@@ -0,0 +1,20 @@
+package com.bharath.jms.messagestructure;
+
+import java.io.Serializable;
+// have to implement serializable to send message
+public class Patient implements Serializable{
+	private int id;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
