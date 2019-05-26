baee79d86f038d3ceee9b447055c5c3d40fe0755
Jimmy Nguyen <nphumy89@gmail.com>
2019-05-27 03:02:45 +1200

    publish subscribe

---
+++ jmsfundamentals/src/main/java/com/bharath/jms/jmsfundamentals/FirstTopic.java
@@ -0,0 +1,44 @@
+package com.bharath.jms.jmsfundamentals;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class FirstTopic {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		InitialContext initialContext= new InitialContext();
+		Topic topic= (Topic)initialContext.lookup("topic/myTopic");
+		
+		ConnectionFactory cf= (ConnectionFactory) initialContext.lookup("ConnectionFactory");
+		Connection connection= cf.createConnection();
+		
+		Session session= connection.createSession();
+		MessageProducer producer= session.createProducer(topic);
+		
+		MessageConsumer consumer1= session.createConsumer(topic);
+		MessageConsumer consumer2= session.createConsumer(topic);
+		
+		TextMessage message= session.createTextMessage("All the power with in me. I can do anything and everything");
+		producer.send(message);
+		
+		connection.start();
+		
+		TextMessage message1= (TextMessage) consumer1.receive();
+		System.out.println("Consumer 1 message received"+message1.getText());
+		TextMessage message2= (TextMessage) consumer2.receive();
+		System.out.println("Consumer 1 message received"+message2.getText());
+		
+		connection.close();
+		initialContext.close();
+	}
+
+}
